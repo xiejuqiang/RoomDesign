b7f36a36ed20b7b4a5fcbe5c3228f1bde0bb5b8c
xiejuqiang <apple@appletekiMac-mini.local>
2013-11-14 15:47:38 +0800

    Initial Commit

---
+++ RoomDesign/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RoomDesign
+//
+//  Created by apple on 13-11-14.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
